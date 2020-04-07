216484bb31263cdc3c20b48f2860b20f2e5837c8
Johan Hagelin <jrhagelin@gmail.com>
2020-04-07 12:54:33 +0200

    Hello rectangle

--- include/graphics.h
+++ include/graphics.h
@@ -474,6 +474,7 @@ public:
 
     unsigned int VBO;
     unsigned int VAO;
+    unsigned int EBO;
 
     GraphicsHandle() {
         
@@ -541,17 +542,30 @@ public:
         glDeleteShader( vertexShader );
         glDeleteShader( fragmentShader );
 
-        float32 vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f
+        float vertices[] = {
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left 
+        };
+        unsigned int indices[] = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
         };
 
+
+        
         glGenBuffers( 1, &VBO );
         glGenVertexArrays(1, &VAO);
+        glGenBuffers( 1, &EBO );
+
         glBindVertexArray( VAO );
+
         glBindBuffer( GL_ARRAY_BUFFER, VBO );
         glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 
         // Right now we sent the input vertex data to the GPU and instructed the GPU how it
         // should process the vertex data within a vertex and fragment shader. We're almost there,
@@ -567,7 +581,7 @@ public:
                               (void*) 0 // Where the position data begins in the buffer.
                               );
         glEnableVertexAttribArray(0); 
-
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     }
 
@@ -589,7 +603,8 @@ public:
 
             glUseProgram(shaderProgram);
             glBindVertexArray(VAO);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            // triangles, 6 vertices, type of index, 0
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
             
 
             if ( history_mode_on ) {
